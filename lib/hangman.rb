cd2d9e3e2ada3ddf46882bc33f3b3b3b78e8ce66
CyberRacc <89751580+CyberRacc@users.noreply.github.com>
2024-03-23 21:37:46 +0000

    Restarted project with better planning and vision

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,83 +1,31 @@
 # frozen-string-literal: true
 
-# Key words:
-# word_teaser - the word with the letters that have been guessed correctly
-# word - the word that the player is trying to guess
-# lives - the number of lives the player has left
-# guess - the letter that the player has guessed
-
-require 'rubocop'
-
 # Hangman class
 class Hangman
   def initialize
-    @user_interaction = UserInteraction.new
-    @word = select_word.split('')
-    @lives = 7
-    @word_teaser = ''
-    @word.length.times do
-      @word_teaser += '_ '
-    end
-    @player = Player.new(@user_interaction.player_name, @lives)
+
+  end
+end
+
+class Word
+  attr_reader :word, :word_teaser
+
+  def initialize
+    @word = random_word
+    @word_teaser = word_teaser
   end
 
-  def play_game
-    puts 'Welcome to Hangman!'
-    puts 'Here is your word:'
-    print_teaser
-  end
-
-  def words_dictionary
-    File.open('google-10000-english-no-swears.txt', 'r') do |file|
-      file.readlines.map(&:strip)
+  def create_teaser(word)
+    word.split('').each_with_index do |char, index|
+      word[index] = '_' if index != 0 && index != word.length - 1
     end
   end
 
-  def select_word
-    possible_words = words_dictionary.select { |word| word.length >= 5 && word.length <= 12 }
-
-    # Select a random word from the possible words
-    possible_words.sample
-  end
-
-  def print_teaser(last_guess = nil)
-    update_teaser(last_guess) unless last_guess.nil?
-    puts @word_teaser
-  end
-
-  def display_progress
-    print_teaser if @word.include?('_')
+  def random_word
+    File.open('/google-10000-english-no-swears.txt', 'r') do |file|
+      all_words = file.readlines
+      selected_word = all_words.select { |word| word.length >= 5 && word.length <= 12 }
+      selected_word.sample
+    end
   end
 end
-
-class UserInteraction
-  def guess_input
-    print 'Enter a letter: '
-    gets.chomp
-  end
-
-  def player_name
-    print 'Enter your name: '
-    gets.chomp
-  end
-
-  def play_again?
-    print 'Do you want to play again? (yes/no): '
-    gets.chomp.downcase == 'yes'
-  end
-
-  def display_lives(lives)
-    puts "You have #{lives} lives left."
-  end
-end
-
-class Player
-  attr_reader :name, :lives
-
-  def initialize(name, lives)
-    @name = name
-    @lives = lives
-  end
-end
-
-Hangman.new
